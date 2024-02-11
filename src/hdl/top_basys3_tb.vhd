1af32202a93cad72b24a1629270cfb02d774db0e
141978638+alexbsick@users.noreply.github.com <c26alexander.sick@afacademy.af.edu>
2024-02-11 15:01:03 -0700

    2/11 Commit

--- src/hdl/top_basys3_tb.vhd
+++ src/hdl/top_basys3_tb.vhd
@@ -47,9 +47,10 @@
 --|    s_<signal name>          = state name
 --|
 --+----------------------------------------------------------------------------
+  
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
 
 entity top_basys3_tb is
 end top_basys3_tb;
@@ -59,12 +60,16 @@ architecture test_bench of top_basys3_tb is
   -- declare the component of your top-level design unit under test (UUT)
   component top_basys3 is
       port(
+        sw : in std_logic_vector(2 downto 0);
+        led : out std_logic_vector(1 downto 0)
           -- TODO
       );
   end component;
   
  
 	-- declare signals needed to stimulate the UUT inputs
+	signal w_sw : std_logic_vector(2 downto 0) := "000";
+	signal w_led : std_logic_vector(1 downto 0) := "00";
 	   -- TODO
 	-- finish declaring needed signals
 begin
@@ -84,12 +89,24 @@ begin
 	
 	    w_sw <= o"0"; wait for 10 ns;
 		assert w_led = "00" report "bad o0" severity failure;
-            w_sw <= o"1"; wait for 10 ns;
-            	assert w_led = "01" report "bad o1" severity failure;
+        w_sw <= o"1"; wait for 10 ns;
+        assert w_led = "01" report "bad o1" severity failure;
+        w_sw <= o"2"; wait for 10 ns;
+        assert w_led = "01" report "bad o2" severity failure;
+        w_sw <= o"3"; wait for 10 ns;
+        assert w_led = "10" report "bad o3" severity failure;
+        w_sw <= o"4"; wait for 10 ns;
+        assert w_led = "01" report "bad o4" severity failure;
+        w_sw <= o"5"; wait for 10 ns;
+        assert w_led = "10" report "bad o5" severity failure;
+        w_sw <= o"6"; wait for 10 ns;
+        assert w_led = "10" report "bad o6" severity failure;
+        w_sw <= o"7"; wait for 10 ns;
+        assert w_led = "11" report "bad o7" severity failure;
 	    --You must fill in the remaining test cases.	
 	
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
 	
-end test_bench;
+end architecture test_bench;
